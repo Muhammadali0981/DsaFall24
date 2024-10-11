7d6b7bd91755b0fbeb01d4837a4aad2c03bb73d4
Muhammadali0981 <muhammadali030605@gmail.com>
2024-10-12 00:27:09 +0500

    done

--- Labs/Lab05/q04.cpp
+++ Labs/Lab05/q04.cpp
@@ -3,16 +3,19 @@
 using namespace std;
 
 
-int binary_sreach(int* arr, int mid, int high, int low, int t){
+int binary_search(int* arr, int low, int high, int t){
     
-    if (arr[mid] == t) return mid;
-    mid = (low + high)/2;
-    if(arr[mid] < t) binary_sreach(arr, mid, high, mid, t);
-    else if(arr[mid] > t) binary_sreach(arr, mid, mid, low, t);
+    if (low > high) return -1;  
+
+    int mid = (low + high) / 2;
+
+    if (arr[mid] == t) return mid;   
+    if (arr[mid] < t) return binary_search(arr, mid + 1, high, t);  
+    else return binary_search(arr, low, mid - 1, t);  
     return -1;
 }
 
 int main(){
     int a[5] = {1, 2, 3, 4, 5};
-    cout << binary_sreach(a, 0, 4, 0, 4) << endl;
+    cout << binary_search(a, 0, 4, 4) << endl;
 }
