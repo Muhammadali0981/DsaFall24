983cfa419b359eb1521a3b62329c988031dd1bb0
Muhammadali0981 <muhammadali030605@gmail.com>
2024-10-14 00:37:53 +0500

    done

--- Labs/Lab06/backtarcking_questions/q01.cpp
+++ Labs/Lab06/backtarcking_questions/q01.cpp
@@ -7,34 +7,17 @@ using namespace std;
 void maze_solution(int arr[4][4], int size, int i, int j, string sol){
 
     
-    if (i == size-1 && j == size-1 ){ cout << sol << endl; return;}
+    if (i == size-1 && j == size-1 ){ cout << sol << endl; sol = ""; return;}
 
+    if(i + 1 > size || i < 0 || j < 0 || j + 1 > size  || arr[i][j] == 0) return;
     arr[i][j] = 0;
 
-    if(i+1 < size  && arr[i+1][j]){
+     
+    maze_solution(arr, 4, i+1, j, sol + "D");
+    maze_solution(arr, 4, i-1, j, sol + "U");
+    maze_solution(arr, 4, i, j+1, sol + "R");    
+    maze_solution(arr, 4, i, j-1, sol + "L");
     
-        sol += "D"; 
-        maze_solution(arr, 4, i+1, j, sol);
-    }
-
-    if(i-1 >= 0 && arr[i-1][j]) {
-
-        sol += "U";
-        maze_solution(arr, 4, i-1, j, sol);
-    }
-
-    if(j+1 < size  && arr[i][j+1]){
-
-        sol += "R";
-        maze_solution(arr, 4, i, j+1, sol);    
-    }
-
-    if(j-1 >= 0 && arr[i][j-1]){
-        
-        sol += "L";
-        maze_solution(arr, 4, i, j-1, sol);
-    }
-
     arr[i][j] = 1;
 }
 
